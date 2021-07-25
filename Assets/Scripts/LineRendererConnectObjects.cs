562a7f96434908e39afdec69d2c238abbcfd21e2
Takashi <bt.tengan@gmail.com>
2021-07-25 17:14:08 -0300

    Creating a LineRenderer that connects multiple objects

---
+++ Assets/Scripts/LineRendererConnectObjects.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LineRendererConnectObjects : MonoBehaviour
+{
+    [SerializeField] List<Transform> _objects = new List<Transform>();
+    LineRenderer _lineRenderer;
+    
+    void Setup() 
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _lineRenderer.positionCount = _objects.Count;
+    }
+
+    void Awake() 
+    {
+        Setup();
+    }
+
+    void Update() 
+    {
+        _lineRenderer.SetPositions(_objects.ConvertAll(__object => __object.position).ToArray());
+    }
+}
