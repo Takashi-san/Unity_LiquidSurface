713e80d5215965e88c81efa489dc1673c17b4e27
Takashi <bt.tengan@gmail.com>
2021-07-25 22:36:11 -0300

    Testing mesh creation and custom inspector

---
+++ Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshGenerator : MonoBehaviour
+{
+    [SerializeField] List<Transform> _objects = new List<Transform>();
+    Mesh _mesh;
+    MeshFilter _meshFilter;
+    Vector3[] _vertex;
+    List<int> _triangles;
+
+    public void Setup()
+    {
+        _mesh = new Mesh();
+        _meshFilter = GetComponent<MeshFilter>();
+        _meshFilter.mesh = _mesh;
+    }
+    
+    public void CreateMesh(bool p_useHandCreated) 
+    {
+        ClearMesh();
+        if (p_useHandCreated) 
+        {
+            HandCreatedMesh();
+        }
+        else
+        {
+            ObjectCreatedMesh();
+        }
+        _mesh.vertices = _vertex;
+        _mesh.triangles = _triangles.ToArray();
+    }
+
+    public void ClearMesh()
+    {
+        _mesh.Clear();
+    }
+
+    void HandCreatedMesh()
+    {
+        _vertex = new Vector3[]
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(0, 1, 0),
+            new Vector3(1, 0, 0)
+        };
+
+        _triangles = new List<int>()
+        {
+            0, 1, 2, // Clockwise triangle.
+            // 2, 1, 0 // Counter clockwise triangle.
+        };
+    }
+
+    void ObjectCreatedMesh() 
+    {
+        _vertex = _objects.ConvertAll(__object => __object.position).ToArray();
+        
+        _triangles.Clear();
+        for (int i = 0; i < _objects.Count - 2; i++)
+        {
+            // Direction triangle.
+            _triangles.Add(i);
+            _triangles.Add(i+1);
+            _triangles.Add(i+2);
+            
+            // Counter direction triangle.
+            // _triangles.Add(i+2);
+            // _triangles.Add(i+1);
+            // _triangles.Add(i);
+        }
+    }
+}
---
+++ Assets/Scripts/MeshGeneratorCustomEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MeshGenerator))]
+public class MeshGeneratorCustomEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MeshGenerator __target = (MeshGenerator)target;
+        DrawDefaultInspector();
+        if (GUILayout.Button("Setup")) 
+        {
+            __target.Setup();
+        }
+        if (GUILayout.Button("Create Mesh by Hand")) 
+        {
+            __target.CreateMesh(true);
+        }
+        if (GUILayout.Button("Create Mesh by Object")) 
+        {
+            __target.CreateMesh(false);
+        }
+        if (GUILayout.Button("Clear")) 
+        {
+            __target.ClearMesh();
+        }
+    }
+}
